af31da236f80b40d09b6cecf9fa783a04ed12279
hrithikraj01 <38440883+hrithikraj01@users.noreply.github.com>
2020-05-29 03:46:32 +0530

    Update app.py

--- flaskapp-docker/flaskapp/app.py
+++ flaskapp-docker/flaskapp/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("home.html")
+def index():return "Hello World"
 
+if __name__ == '__main__': 
+    app.run()
